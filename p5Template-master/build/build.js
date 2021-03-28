f503af476b97cf0c3c4c9a2877af73c45ff40955
Théo Arnauld des lions <45674993+Dyokiin@users.noreply.github.com>
2021-03-28 19:38:19 +0200

    Final Version

--- p5Template-master/build/build.js
+++ p5Template-master/build/build.js
@@ -1,41 +1,66 @@
 var gui = new dat.GUI();
 var params = {
     Download_ImaG3a: function () { return save(); },
+    Random_seed: 673,
+    Lines: false,
+    Random_lines: false,
 };
 gui.add(params, "Download_ImaG3a");
+gui.add(params, "Random_seed", 0, 10000, 1);
+gui.add(params, "Random_lines");
+gui.add(params, "Lines", 0, 1, 1);
 function draw() {
+    square(0, 0, width);
+    randomSeed(params.Random_seed);
+    var Random_lines = 0;
+    if (params.Random_lines) {
+        Random_lines = 1;
+    }
     var u = (width / 35);
-    var ellipse = [6.5 * u, 5 * u];
-    var G1a = [17, 0];
-    var G1b = [12, 35];
-    var G2a = [17, 0];
-    var G2b = [20, 35];
-    var G3a = [18, 0];
-    var G3b = [16, 35];
-    var G4a = [0, 15];
-    var G4b = [35, 18];
-    var G5a = [0, 18];
-    var G5b = [35, 18];
-    var P11a = [14, 0];
-    var P11b = [14, 35];
-    var P12a = [16, 0];
-    var P12b = [16, 35];
-    var P21a = [0, 17];
-    var P21b = [35, 15];
-    var P22a = [0, 19];
-    var P22b = [35, 17];
-    var P31a = [0, 4];
-    var P31b = [33, 35];
-    var P32a = [0, 2];
-    var P32b = [35, 35];
-    var P33a = [1, 0];
-    var P33b = [35, 33];
-    var P41a = [31, 0];
-    var P41b = [0, 31];
-    var P42a = [33, 0];
-    var P42b = [0, 33];
-    var P43a = [35, 0];
-    var P43b = [0, 35];
+    var G1a = [17 + Random_lines * random(-17, 18), 0 + Random_lines * random(0, 35)];
+    var G1b = [12 + Random_lines * random(-12, 23), 35 + Random_lines * random(-35, 0)];
+    var G2a = [17 + Random_lines * random(-17, 18), 0 + Random_lines * random(0, 35)];
+    var G2b = [20 + Random_lines * random(-20, 15), 35 + Random_lines * random(-35, 0)];
+    var G3a = [18 + Random_lines * random(-17, 17), 0 + Random_lines * random(0, 35)];
+    var G3b = [16 + Random_lines * random(-16, 19), 35 + Random_lines * random(-35, 0)];
+    var G4a = [0 + Random_lines * random(0, 35), 15 + Random_lines * random(-15, 20)];
+    var G4b = [35 + Random_lines * random(-35, 0), 18 + Random_lines * random(-18, 17)];
+    var G5a = [0 + Random_lines * random(0, 35), 18 + Random_lines * random(-18, 17)];
+    var G5b = [35 + Random_lines * random(-35, 0), 18 + Random_lines * random(-18, 17)];
+    var random1 = Random_lines * random(-14, 21);
+    var random2 = Random_lines * random(-16, 19);
+    var P11a = [13 + random1, 0];
+    var P11b = [15 + random2, 35];
+    var P12a = [15 + random1, 0];
+    var P12b = [17 + random2, 35];
+    random1 = Random_lines * random(-16, 19);
+    random2 = Random_lines * random(-15, 20);
+    var P21a = [0, 16 + random1];
+    var P21b = [35, 15 + random2];
+    var P22a = [0, 17 + random1];
+    var P22b = [35, 16 + random1];
+    random1 = Random_lines * random(-2, 33);
+    random2 = Random_lines * random(-35, 0);
+    var P31a = [0, 4 + random1];
+    var P31b = [33, 35 + random2];
+    var P32a = [0, 2 + random1];
+    var P32b = [35, 35 + random2];
+    var P33a = [1, 0 + random1];
+    var P33b = [35, 33 + random2];
+    random1 = Random_lines * random(-33, 2);
+    random2 = Random_lines * random(-33, 2);
+    var P41a = [31 + random1, 0];
+    var P41b = [0, 31 + random2];
+    var P42a = [33 + random1, 0];
+    var P42b = [0, 33 + random2];
+    var P43a = [35 + random1, 0];
+    var P43b = [0, 35 + random2];
+    random1 = Random_lines * random(-20, 15);
+    random2 = Random_lines * random(-32, 3);
+    var P51a = [19 + random1, 0];
+    var P51b = [0, 30 + random2];
+    var P52a = [20 + random1, 0];
+    var P52b = [0, 32 + random2];
     var G1 = [(G1b[1] - G1a[1]) / (G1b[0] - G1a[0]), G1a[1] - (((G1b[1] - G1a[1]) / (G1b[0] - G1a[0])) * G1a[0])];
     var G2 = [(G2b[1] - G2a[1]) / (G2b[0] - G2a[0]), G2a[1] - (((G2b[1] - G2a[1]) / (G2b[0] - G2a[0])) * G2a[0])];
     var G3 = [(G3b[1] - G3a[1]) / (G3b[0] - G3a[0]), G3a[1] - (((G3b[1] - G3a[1]) / (G3b[0] - G3a[0])) * G3a[0])];
@@ -51,7 +76,9 @@ function draw() {
     var P41 = [(P41b[1] - P41a[1]) / (P41b[0] - P41a[0]), P41a[1] - (((P41b[1] - P41a[1]) / (P41b[0] - P41a[0])) * P41a[0])];
     var P42 = [(P42b[1] - P42a[1]) / (P42b[0] - P42a[0]), P42a[1] - (((P42b[1] - P42a[1]) / (P42b[0] - P42a[0])) * P42a[0])];
     var P43 = [(P43b[1] - P43a[1]) / (P43b[0] - P43a[0]), P43a[1] - (((P43b[1] - P43a[1]) / (P43b[0] - P43a[0])) * P43a[0])];
-    var Droites = [G1, G2, G3, G4, G5, P11, P12, P21, P22, P31, P32, P33, P41, P42, P43];
+    var P51 = [(P51b[1] - P51a[1]) / (P51b[0] - P51a[0]), P51a[1] - (((P51b[1] - P51a[1]) / (P51b[0] - P51a[0])) * P51a[0])];
+    var P52 = [(P52b[1] - P52a[1]) / (P52b[0] - P52a[0]), P52a[1] - (((P52b[1] - P52a[1]) / (P52b[0] - P52a[0])) * P52a[0])];
+    var Droites = [G1, G2, G3, G4, G5, P11, P12, P21, P22, P31, P32, P33, P41, P42, P43, P51, P52];
     for (var y = 0; y <= width; y++) {
         stroke(53, 105, 157);
         line(0, y, (y - G1[1] * u) / G1[0], y);
@@ -70,20 +97,44 @@ function draw() {
             line(width, y, (y - G3[1] * u) / G3[0], y);
         }
     }
-    for (var x = -ellipse[0] + 14 * u; x <= ellipse[0] + 14 * u; x++) {
-        for (var y = -ellipse[1] + 16 * u; y <= ellipse[1] + 16 * u; y++) {
+    stroke(random(100, 255), random(100, 150), random(100, 200));
+    for (var x = (P33[1] * u - P41[1] * u) / (P41[0] - P33[0]); x <= (P33[1] * u - P42[1] * u) / (P42[0] - P33[0]); x += 0.5) {
+        line(x, P33[0] * x + P33[1] * u, x - ((P33[1] * u - P41[1] * u) / (P41[0] - P33[0]) - (P32[1] * u - P41[1] * u) / (P41[0] - P32[0])), P32[0] * (x - ((P33[1] * u - P41[1] * u) / (P41[0] - P33[0]) - (P32[1] * u - P41[1] * u) / (P41[0] - P32[0]))) + (P32[1] * u));
+    }
+    stroke(random(0, 155), random(0, 50), random(0, 100));
+    for (var x = (P33[1] * u - P42[1] * u) / (P42[0] - P33[0]); x <= (P33[1] * u - P43[1] * u) / (P43[0] - P33[0]); x += 0.5) {
+        line(x, P33[0] * x + P33[1] * u, x - ((P33[1] * u - P42[1] * u) / (P42[0] - P33[0]) - (P32[1] * u - P42[1] * u) / (P42[0] - P32[0])), P32[0] * (x - ((P33[1] * u - P42[1] * u) / (P42[0] - P33[0]) - (P32[1] * u - P42[1] * u) / (P42[0] - P32[0]))) + (P32[1] * u));
+    }
+    stroke(0, 0, 0);
+    for (var y = P32[0] * ((P12[1] * u - P42[1] * u) / (P42[0] - P12[0]) + P32[1] * u); y <= width; y += 0.5) {
+        line((y - P11[1] * u) / P11[0], y, (y - P12[1] * u) / P12[0], y);
+    }
+    stroke(random(50, 100), random(100, 150), random(200, 255));
+    for (var x = (P31[1] * u - P21[1] * u) / (P21[0] - P31[0]); x <= (P11[1] * u - P21[1] * u) / (P21[0] - P11[0]); x += 0.5) {
+        line(x, P31[0] * x + P31[1] * u, x - ((P31[1] * u - P21[1] * u) / (P21[0] - P31[0]) - (P31[1] * u - P21[1] * u) / (P21[0] - P31[0])), x * P21[0] + P22[1] * u);
+    }
+    stroke(random(100, 255), random(100, 150), random(100, 200));
+    for (var x = (P32[1] * u - P51[1] * u) / (P51[0] - P32[0]); x <= (P32[1] * u - P52[1] * u) / (P52[0] - P32[0]); x += 0.5) {
+        line(x, P32[0] * x + P32[1] * u, x - ((P32[1] * u - P51[1] * u) / (P51[0] - P32[0]) - (P31[1] * u - P51[1] * u) / (P51[0] - P31[0])), P31[0] * (x - ((P32[1] * u - P51[1] * u) / (P51[0] - P32[0]) - (P31[1] * u - P51[1] * u) / (P51[0] - P31[0]))) + (P31[1] * u));
+    }
+    stroke(random(100, 255), random(100, 150), random(100, 200));
+    for (var x = (P31[1] * u - P51[1] * u) / (P51[0] - P31[0]); x <= (P31[1] * u - P52[1] * u) / (P52[0] - P31[0]); x += 0.5) {
+        line(x, P31[0] * x + P31[1] * u, (P21[1] * u - P51[1] * u) / (P51[0] - P21[0]), P21[0] * (P21[1] * u - P51[1] * u) / (P51[0] - P21[0]) + (P21[1] * u));
+    }
+    stroke(random(100, 155), random(100, 250), random(100, 200));
+    for (var x = (P21[1] * u - P51[1] * u) / (P51[0] - P21[0]); x <= (P21[1] * u - P52[1] * u) / (P52[0] - P21[0]); x += 0.5) {
+        line(x, P21[0] * x + P21[1] * u, x - ((P21[1] * u - P51[1] * u) / (P51[0] - P21[0]) - (P22[1] * u - P51[1] * u) / (P51[0] - P22[0])), P22[0] * (x - ((P21[1] * u - P51[1] * u) / (P51[0] - P21[0]) - (P22[1] * u - P51[1] * u) / (P51[0] - P22[0]))) + (P22[1] * u));
+    }
+    stroke(random(100, 255), random(100, 200), random(10, 100));
+    for (var y = P31[0] * ((P21[1] * u - P31[1] * u) / (P31[0] - P21[0])) + P31[1] * u; y <= P22[0] * ((P21[1] * u - P31[1] * u) / (P31[0] - P21[0])) + P22[1] * u; y += 0.5) {
+        line((P21[1] * u - P52[1] * u) / (P52[0] - P21[0]), P21[0] * ((P21[1] * u - P51[1] * u) / (P51[0] - P21[0])) + P21[1] * u, (P21[1] * u - P31[1] * u) / (P31[0] - P21[0]), y);
+    }
+    if (params.Lines) {
+        stroke(255, 0, 0);
+        for (var i = 5; i <= 16; i++) {
+            line((0 - Droites[i][1] * u) / Droites[i][0], 0, (width - Droites[i][1] * u) / Droites[i][0], width);
         }
     }
-    stroke(255, 0, 0);
-    for (var i = 5; i <= 15; i++) {
-        line((0 - Droites[i][1] * u) / Droites[i][0], 0, (width - Droites[i][1] * u) / Droites[i][0], width);
-    }
-    noFill();
-    beginShape();
-    for (var t = 0; t <= TWO_PI; t += 0.05) {
-        vertex((ellipse[0] * cos(t)) + 14 * u, (ellipse[1] * sin(t)) + 16 * u);
-    }
-    endShape(CLOSE);
 }
 function setup() {
     p6_CreateCanvas();
--- p5Template-master/src/sketch.ts
+++ p5Template-master/src/sketch.ts
@@ -5,136 +5,177 @@
 const gui = new dat.GUI()
 const params = {
     Download_ImaG3a: () => save(),
+    Random_seed : 673,
+    Lines : false,
+    Random_lines : false,
 }
 
 gui.add(params, "Download_ImaG3a")
-
+gui.add(params, "Random_seed", 0, 10000,1)
+gui.add(params, "Random_lines")
+gui.add(params, "Lines", 0, 1, 1)
 
 // -------------------
 //       Drawing
 // -------------------
 
-
 function draw() {
+    square(0,0,width)
+    randomSeed(params.Random_seed)
 
-    let u = (width/35)
+    let Random_lines = 0
+    if(params.Random_lines){Random_lines = 1}
 
-    let ellipse = [6.5*u,5*u]
+    const u = (width/35)
 
-    let G1a = [17,0]
-    let G1b = [12,35]
-    let G2a = [17,0]
-    let G2b = [20,35]
-    let G3a = [18,0]
-    let G3b = [16,35]
-    let G4a = [0,15]
-    let G4b = [35,18]
-    let G5a = [0,18]
-    let G5b = [35,18]
+    //Définition des points, éventuellement avec une position aléatoire 
 
-    let P11a = [14,0]
-    let P11b = [14,35]
-    let P12a = [16,0]
-    let P12b = [16,35]
+    const G1a = [17+Random_lines*random(-17,18),0+Random_lines*random(0,35)]
+    const G1b = [12+Random_lines*random(-12,23),35+Random_lines*random(-35,0)]
+    const G2a = [17+Random_lines*random(-17,18),0+Random_lines*random(0,35)]
+    const G2b = [20+Random_lines*random(-20,15),35+Random_lines*random(-35,0)]
+    const G3a = [18+Random_lines*random(-17,17),0+Random_lines*random(0,35)]
+    const G3b = [16+Random_lines*random(-16,19),35+Random_lines*random(-35,0)]
+    const G4a = [0+Random_lines*random(0,35),15+Random_lines*random(-15,20)]
+    const G4b = [35+Random_lines*random(-35,0),18+Random_lines*random(-18,17)]
+    const G5a = [0+Random_lines*random(0,35),18+Random_lines*random(-18,17)]
+    const G5b = [35+Random_lines*random(-35,0),18+Random_lines*random(-18,17)]
 
-    let P21a = [0,17]
-    let P21b = [35,15]
-    let P22a = [0,19]
-    let P22b = [35,17]
+    let random1 = Random_lines*random(-14, 21)
+    let random2 = Random_lines*random(-16, 19)
+    const P11a = [13+random1,0]
+    const P11b = [15+random2,35]
+    const P12a = [15+random1,0]
+    const P12b = [17+random2,35]
 
-    let P31a = [0,4]
-    let P31b = [33,35]
-    let P32a = [0,2]
-    let P32b = [35,35]
-    let P33a = [1,0]
-    let P33b = [35,33]
+    random1 = Random_lines*random(-16, 19)
+    random2 = Random_lines*random(-15, 20)
+    const P21a = [0,16+random1]
+    const P21b = [35,15+random2]
+    const P22a = [0,17+random1]
+    const P22b = [35,16+random1]
 
-    let P41a = [31,0]
-    let P41b = [0,31]
-    let P42a = [33,0]
-    let P42b = [0,33]
-    let P43a = [35,0]
-    let P43b = [0,35]
+    random1 = Random_lines*random(-2, 33)
+    random2 = Random_lines*random(-35, 0)
+    const P31a = [0,4+random1]
+    const P31b = [33,35+random2]
+    const P32a = [0,2+random1]
+    const P32b = [35,35+random2]
+    const P33a = [1,0+random1]
+    const P33b = [35,33+random2]
 
-    //let P51a = [random(0,35), random(0,35)]
-    //let P51b = 
+    random1 = Random_lines*random(-33, 2)
+    random2 = Random_lines*random(-33, 2)
+    const P41a = [31+random1,0]
+    const P41b = [0,31+random2]
+    const P42a = [33+random1,0]
+    const P42b = [0,33+random2]
+    const P43a = [35+random1,0]
+    const P43b = [0,35+random2]
+
+    random1 = Random_lines*random(-20, 15)
+    random2 = Random_lines*random(-32, 3)
+    const P51a = [19+random1,0]
+    const P51b = [0,30+random2]
+    const P52a = [20+random1,0]
+    const P52b = [0,32+random2]
   
+    // Gn = [ xb - xa / yb - ya , ya - coefficient directeur *  xa]  //Définition des équations de droite
 
-// Gn = [ xb - xa / yb - ya , ya - coefficient directeur *  xa]  //Définition des équations de droite
+    const G1 = [(G1b[1]-G1a[1])/(G1b[0]-G1a[0]), G1a[1]-(((G1b[1]-G1a[1])/(G1b[0]-G1a[0]))*G1a[0])]
+    const G2 = [(G2b[1]-G2a[1])/(G2b[0]-G2a[0]), G2a[1]-(((G2b[1]-G2a[1])/(G2b[0]-G2a[0]))*G2a[0])]
+    const G3 = [(G3b[1]-G3a[1])/(G3b[0]-G3a[0]), G3a[1]-(((G3b[1]-G3a[1])/(G3b[0]-G3a[0]))*G3a[0])]
+    const G4 = [(G4b[1]-G4a[1])/(G4b[0]-G4a[0]), G4a[1]-(((G4b[1]-G4a[1])/(G4b[0]-G4a[0]))*G4a[0])]
+    const G5 = [(G5b[1]-G5a[1])/(G5b[0]-G5a[0]), G5a[1]-(((G5b[1]-G5a[1])/(G5b[0]-G5a[0]))*G5a[0])]
 
-    let G1 = [(G1b[1]-G1a[1])/(G1b[0]-G1a[0]), G1a[1]-(((G1b[1]-G1a[1])/(G1b[0]-G1a[0]))*G1a[0])]
-    let G2 = [(G2b[1]-G2a[1])/(G2b[0]-G2a[0]), G2a[1]-(((G2b[1]-G2a[1])/(G2b[0]-G2a[0]))*G2a[0])]
-    let G3 = [(G3b[1]-G3a[1])/(G3b[0]-G3a[0]), G3a[1]-(((G3b[1]-G3a[1])/(G3b[0]-G3a[0]))*G3a[0])]
-    let G4 = [(G4b[1]-G4a[1])/(G4b[0]-G4a[0]), G4a[1]-(((G4b[1]-G4a[1])/(G4b[0]-G4a[0]))*G4a[0])]
-    let G5 = [(G5b[1]-G5a[1])/(G5b[0]-G5a[0]), G5a[1]-(((G5b[1]-G5a[1])/(G5b[0]-G5a[0]))*G5a[0])]
-
-    let P11 = [(P11b[1]-P11a[1])/(P11b[0]-P11a[0]), P11a[1]-(((P11b[1]-P11a[1])/(P11b[0]-P11a[0]))*P11a[0])]
-    let P12 = [(P12b[1]-P12a[1])/(P12b[0]-P12a[0]), P12a[1]-(((P12b[1]-P12a[1])/(P12b[0]-P12a[0]))*P12a[0])]
+    const P11 = [(P11b[1]-P11a[1])/(P11b[0]-P11a[0]), P11a[1]-(((P11b[1]-P11a[1])/(P11b[0]-P11a[0]))*P11a[0])]
+    const P12 = [(P12b[1]-P12a[1])/(P12b[0]-P12a[0]), P12a[1]-(((P12b[1]-P12a[1])/(P12b[0]-P12a[0]))*P12a[0])]
     
-    let P21 = [(P21b[1]-P21a[1])/(P21b[0]-P21a[0]), P21a[1]-(((P21b[1]-P21a[1])/(P21b[0]-P21a[0]))*P21a[0])]
-    let P22 = [(P22b[1]-P22a[1])/(P22b[0]-P22a[0]), P22a[1]-(((P22b[1]-P22a[1])/(P22b[0]-P22a[0]))*P22a[0])]
+    const P21 = [(P21b[1]-P21a[1])/(P21b[0]-P21a[0]), P21a[1]-(((P21b[1]-P21a[1])/(P21b[0]-P21a[0]))*P21a[0])]
+    const P22 = [(P22b[1]-P22a[1])/(P22b[0]-P22a[0]), P22a[1]-(((P22b[1]-P22a[1])/(P22b[0]-P22a[0]))*P22a[0])]
 
-    let P31 = [(P31b[1]-P31a[1])/(P31b[0]-P31a[0]), P31a[1]-(((P31b[1]-P31a[1])/(P31b[0]-P31a[0]))*P31a[0])]
-    let P32 = [(P32b[1]-P32a[1])/(P32b[0]-P32a[0]), P32a[1]-(((P32b[1]-P32a[1])/(P32b[0]-P32a[0]))*P32a[0])]
-    let P33 = [(P33b[1]-P33a[1])/(P33b[0]-P33a[0]), P33a[1]-(((P33b[1]-P33a[1])/(P33b[0]-P33a[0]))*P33a[0])]
+    const P31 = [(P31b[1]-P31a[1])/(P31b[0]-P31a[0]), P31a[1]-(((P31b[1]-P31a[1])/(P31b[0]-P31a[0]))*P31a[0])]
+    const P32 = [(P32b[1]-P32a[1])/(P32b[0]-P32a[0]), P32a[1]-(((P32b[1]-P32a[1])/(P32b[0]-P32a[0]))*P32a[0])]
+    const P33 = [(P33b[1]-P33a[1])/(P33b[0]-P33a[0]), P33a[1]-(((P33b[1]-P33a[1])/(P33b[0]-P33a[0]))*P33a[0])]
 
-    let P41 = [(P41b[1]-P41a[1])/(P41b[0]-P41a[0]), P41a[1]-(((P41b[1]-P41a[1])/(P41b[0]-P41a[0]))*P41a[0])]
-    let P42 = [(P42b[1]-P42a[1])/(P42b[0]-P42a[0]), P42a[1]-(((P42b[1]-P42a[1])/(P42b[0]-P42a[0]))*P42a[0])]
-    let P43 = [(P43b[1]-P43a[1])/(P43b[0]-P43a[0]), P43a[1]-(((P43b[1]-P43a[1])/(P43b[0]-P43a[0]))*P43a[0])]
+    const P41 = [(P41b[1]-P41a[1])/(P41b[0]-P41a[0]), P41a[1]-(((P41b[1]-P41a[1])/(P41b[0]-P41a[0]))*P41a[0])]
+    const P42 = [(P42b[1]-P42a[1])/(P42b[0]-P42a[0]), P42a[1]-(((P42b[1]-P42a[1])/(P42b[0]-P42a[0]))*P42a[0])]
+    const P43 = [(P43b[1]-P43a[1])/(P43b[0]-P43a[0]), P43a[1]-(((P43b[1]-P43a[1])/(P43b[0]-P43a[0]))*P43a[0])]
 
+    const P51 = [(P51b[1]-P51a[1])/(P51b[0]-P51a[0]), P51a[1]-(((P51b[1]-P51a[1])/(P51b[0]-P51a[0]))*P51a[0])]
+    const P52 = [(P52b[1]-P52a[1])/(P52b[0]-P52a[0]), P52a[1]-(((P52b[1]-P52a[1])/(P52b[0]-P52a[0]))*P52a[0])]
 
-    let Droites = [G1,G2,G3,G4,G5,  P11,P12,  P21,P22,  P31,P32,P33,  P41,P42,P43]
+    let Droites = [G1,G2,G3,G4,G5,  P11,P12,  P21,P22,  P31,P32,P33,  P41,P42,P43,  P51,P52]
 
+    //Dessin des trois Zones principales
 
     for(let y=0; y<=width; y++){
         
-        stroke(53,105,157) //Zone Principale
+        stroke(53,105,157) //Zone bleue
         line(0, y, (y - G1[1]*u)/G1[0], y) 
 
-        stroke(200,223,213) //Zone Haut Droit
-        if(y<=G2[0]*((G4[1]*u-G2[1]*u)/(G2[0]-G4[0]))+G2[1]*u){  //intersection des deux droites
+        stroke(200,223,213) //Zone verte
+        if(y<=G2[0]*((G4[1]*u-G2[1]*u)/(G2[0]-G4[0]))+G2[1]*u){  //intersection des deux droites en x = (Coo2 - Coo1) / (coefd2 - coefd1)
             line(width, y, (y-G2[1]*u)/G2[0], y)
         } else {
             line(width, y, (y-G4[1]*u)/G4[0], y)
         }
 
-        stroke(222,222,235) //Zone Bas Droit
+        stroke(222,222,235) //Zone mauve
         if(y<=G3[0]*((G5[1]*u-G3[1]*u)/(G3[0]-G5[0]))+G3[1]*u){
             line(width, y, (y - G5[1]*u)/G5[0], y)
         } else {
             line(width, y, (y - G3[1]*u)/G3[0], y)
         }    
-    } 
+    }
 
-    for(let x= -ellipse[0]+14*u; x<= ellipse[0]+14*u; x++){
-        for(let y= -ellipse[1]+16*u; y<=ellipse[1]+16*u; y++){
-            
+    //Dessin des petites zones colorées
+
+    //x d'intersection = (Droites[i+1][1]-Droites[i][1])/(Droites[i][0]-Droites[i+1][0])
+    //y d'intersection = f(xi)
+
+    stroke(random(100,255), random(100,150), random(100,200))
+    for(let  x = (P33[1]*u-P41[1]*u)/(P41[0]-P33[0]); x<=(P33[1]*u-P42[1]*u)/(P42[0]-P33[0]); x+=0.5){
+        line(x, P33[0]*x+P33[1]*u, x-((P33[1]*u-P41[1]*u)/(P41[0]-P33[0])-(P32[1]*u-P41[1]*u)/(P41[0]-P32[0])), P32[0]*(x-((P33[1]*u-P41[1]*u)/(P41[0]-P33[0])-(P32[1]*u-P41[1]*u)/(P41[0]-P32[0])))+(P32[1]*u))
+    }
+    stroke(random(0,155), random(0,50), random(0,100))
+    for(let  x = (P33[1]*u-P42[1]*u)/(P42[0]-P33[0]); x<=(P33[1]*u-P43[1]*u)/(P43[0]-P33[0]); x+=0.5){
+        line(x, P33[0]*x+P33[1]*u, x-((P33[1]*u-P42[1]*u)/(P42[0]-P33[0])-(P32[1]*u-P42[1]*u)/(P42[0]-P32[0])), P32[0]*(x-((P33[1]*u-P42[1]*u)/(P42[0]-P33[0])-(P32[1]*u-P42[1]*u)/(P42[0]-P32[0])))+(P32[1]*u))
+    }
+    stroke(0, 0, 0)
+    for(let y = P32[0]*((P12[1]*u-P42[1]*u)/(P42[0]-P12[0])+P32[1]*u); y<=width; y+=0.5){
+        line((y-P11[1]*u)/P11[0], y, (y-P12[1]*u)/P12[0], y)
+    }
+    stroke(random(50,100), random(100,150), random(200,255))
+    for(let  x = (P31[1]*u-P21[1]*u)/(P21[0]-P31[0]); x<=(P11[1]*u-P21[1]*u)/(P21[0]-P11[0]); x+=0.5){
+        line(x, P31[0]*x+P31[1]*u, x-((P31[1]*u-P21[1]*u)/(P21[0]-P31[0])-(P31[1]*u-P21[1]*u)/(P21[0]-P31[0])), x*P21[0]+P22[1]*u)
+    }
+    stroke(random(100,255), random(100,150), random(100,200))
+    for(let  x = (P32[1]*u-P51[1]*u)/(P51[0]-P32[0]); x<=(P32[1]*u-P52[1]*u)/(P52[0]-P32[0]); x+=0.5){
+        line(x, P32[0]*x+P32[1]*u, x-((P32[1]*u-P51[1]*u)/(P51[0]-P32[0])-(P31[1]*u-P51[1]*u)/(P51[0]-P31[0])), P31[0]*(x-((P32[1]*u-P51[1]*u)/(P51[0]-P32[0])-(P31[1]*u-P51[1]*u)/(P51[0]-P31[0])))+(P31[1]*u))
+    }
+    stroke(random(100,255), random(100,150), random(100,200))
+    for(let  x = (P31[1]*u-P51[1]*u)/(P51[0]-P31[0]); x<=(P31[1]*u-P52[1]*u)/(P52[0]-P31[0]); x+=0.5){
+        line(x, P31[0]*x+P31[1]*u, (P21[1]*u-P51[1]*u)/(P51[0]-P21[0]), P21[0]*(P21[1]*u-P51[1]*u)/(P51[0]-P21[0])+(P21[1]*u))
+    }
+    stroke(random(100,155), random(100,250), random(100,200))
+    for(let  x = (P21[1]*u-P51[1]*u)/(P51[0]-P21[0]); x<=(P21[1]*u-P52[1]*u)/(P52[0]-P21[0]); x+=0.5){
+        line(x, P21[0]*x+P21[1]*u, x-((P21[1]*u-P51[1]*u)/(P51[0]-P21[0])-(P22[1]*u-P51[1]*u)/(P51[0]-P22[0])), P22[0]*(x-((P21[1]*u-P51[1]*u)/(P51[0]-P21[0])-(P22[1]*u-P51[1]*u)/(P51[0]-P22[0])))+(P22[1]*u))
+    }
+    stroke(random(100, 255), random(100, 200), random(10, 100))
+    for(let y = P31[0]*((P21[1]*u-P31[1]*u)/(P31[0]-P21[0]))+P31[1]*u; y<=P22[0]*((P21[1]*u-P31[1]*u)/(P31[0]-P21[0]))+P22[1]*u; y+=0.5){
+        line((P21[1]*u-P52[1]*u)/(P52[0]-P21[0]), P21[0]*((P21[1]*u-P51[1]*u)/(P51[0]-P21[0]))+P21[1]*u, (P21[1]*u-P31[1]*u)/(P31[0]-P21[0]), y)
+    }
+
+    //Affichage éventuellement des lignes et des axes de construction
+
+    if(params.Lines){
+        stroke(255,0,0)
+
+        for(let i=5; i<=16; i++){
+            line((0-Droites[i][1]*u)/Droites[i][0], 0, (width-Droites[i][1]*u)/Droites[i][0], width)
         }
     }
-
-
-
-    stroke(255,0,0)
-
-    for(let i=5; i<=15; i++){
-        line((0-Droites[i][1]*u)/Droites[i][0], 0, (width-Droites[i][1]*u)/Droites[i][0], width)
-    }
-
-    noFill()
-    beginShape()
-    for(let t=0; t<=TWO_PI;t+=0.05){
-        vertex((ellipse[0]*cos(t)) + 14*u, (ellipse[1]*sin(t)) + 16*u)
-    }
-    endShape(CLOSE)
-
-
-
-    // stroke(0,0,0)
-    // line(G1a[0]*u, G1a[1]*u, G1b[0]*u, G1b[1]*u)
-    // line(G2a[0]*u, G2a[1]*u, G2b[0]*u, G2b[1]*u)
-    // line(G3a[0]*u, G3a[1]*u, G3b[0]*u, G3b[1]*u)
-    // line(G4a[0]*u, G4a[1]*u, G4b[0]*u, G4b[1]*u)
-    // line(G5a[0]*u, G5a[1]*u, G5b[0]*u, G5b[1]*u)
 }
 
 // -------------------
